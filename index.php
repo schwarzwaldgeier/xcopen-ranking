300f6a7c91c792ac3a0efe0863ebc588244f8f16
Sebastian Schmied <sebastian.schmied@burda.com>
2022-06-01 15:52:27 +0200

    cosmetics

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
-const FILTER_PARAMS = "d0=26.05.2022&d1=19.08.2022&fkcat%5B%5D=1&l-fkcat%5B%5D=Gleitschirm&fkto%5B%5D=9543&l-fkto%5B%5D=Merkur%20DE&navpars=%7B%22start%22%3A0%2C%22limit%22%3A500%2C%22sort%22%3A%5B%7B%22field%22%3A%22BestTaskPoints%22%2C%22dir%22%3A1%7D%2C%7B%22field%22%3A%22BestTaskSpeed%22%2C%22dir%22%3A1%7D%5D%7D";
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$arrContextOptions=array(
-    "ssl"=>array(
-        "verify_peer"=>false,
-        "verify_peer_name"=>false,
+const FILTER_PARAMS = "d0=26.05.2022&d1=19.08.2022&fkcat%5B%5D=1&l-fkcat%5B%5D=Gleitschirm&fkto%5B%5D=9543&l-fkto%5B%5D=Merkur%20DE&navpars=%7B%22start%22%3A0%2C%22limit%22%3A500%2C%22sort%22%3A%5B%7B%22field%22%3A%22BestTaskPoints%22%2C%22dir%22%3A1%7D%2C%7B%22field%22%3A%22BestTaskSpeed%22%2C%22dir%22%3A1%7D%5D%7D";
+
+
+$arrContextOptions = array(
+    "ssl" => array(
+        "verify_peer" => false,
+        "verify_peer_name" => false,
     ),
 );
 
-$url = "https://de.dhv-xc.de/api/fli/flights?" . FILTER_PARAMS ;
+$url = "https://de.dhv-xc.de/api/fli/flights?" . FILTER_PARAMS;
 $response = file_get_contents($url, false, stream_context_create($arrContextOptions));
 
 $responseJson = json_decode($response);
@@ -49,15 +51,6 @@ $participantIds = [
     "10446", //Wibke Ziegler
     "7542", //Kraft
     "566", //Kadalla
-
-
-
-
-
-
-
-
-
 
 
 ];
@@ -101,80 +94,90 @@ usort($pilots, function ($a, $b) {
     return $b->totalPoints - $a->totalPoints;
 
 });
-?>
-    <!doctype html>
+$airtimeSymbol = "🕰️";
+if (time() % 10 === 0) {
+    $airtimeSymbol = "🥱";
+}
 
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
-        <title>XC open</title>
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex">
+    <link rel="stylesheet" href="style.css">
+    <title>XC open Ranking</title>
+</head>
+
+<body>
+<h2>XC Open 2022 Live Ranking</h2>
+<table>
+    <tr>
+        <th title="Platzierung">🏅</th>
+        <th title="Pilot">🪂</th>
+        <th title="One-Way">💨</th>
+        <th title="Dreieck">📐</th>
+        <th title="Airtime"><?php echo $airtimeSymbol; ?></th>
+        <th title="Summe">Σ</th>
+    </tr>
+    <?php
+
+    $flightUrl = "https://de.dhv-xc.de/flight/";
+    $rank = 0;
+    foreach ($pilots as $pilot) {
+        $rank++;
+        $displayRank = $rank;
+        if ($rank === 1) {
+            $displayRank = "🥇";
+        } else if ($rank === 2) {
+            $displayRank = "🥈";
+        } else if ($rank === 3) {
+            $displayRank = "🥉";
+        }
+        $totalScore = $pilot->totalPoints;
+        $name = $pilot->name;
+
+        $airtimeScore = $pilot->airtimePoints;
+        $triangleScore = $pilot->trianglePoints;
+        $distanceScore = $pilot->distancePoints;
+        $distanceUrl = "#";
+        $triangleUrl = "#";
+        $airtimeUrl = "#";
+
+        if (!empty($pilot->distancePointsFlight)) {
+            $distanceUrl = $flightUrl . $pilot->distancePointsFlight;
+        }
+
+        if (!empty($pilot->trianglePointsFlight)) {
+            $triangleUrl = $flightUrl . $pilot->trianglePointsFlight;
+        }
+
+        if (!empty($pilot->airtimePointsFlight)) {
+            $airtimeUrl = $flightUrl . $pilot->airtimePointsFlight;
+        }
+
+        $distanceCell = "<td><a href=\"$distanceUrl\">$distanceScore</a></td>";
+        if ($distanceUrl === "#") {
+            $distanceCell = "<td>$distanceScore</td>";
+        }
+
+        $triangleCell = "<td><a href=\"$triangleUrl\">$triangleScore</a></td>";
+
+        if ($triangleUrl === "#") {
+            $triangleCell = "<td>$triangleScore</td>";
+        }
+        $airtimeCell = "<td><a href=\"$airtimeUrl\">$airtimeScore</a></td>";
+
+        if ($airtimeUrl === "#") {
+            $airtimeCell = "<td>$airtimeScore</td>";
+        }
+
+        $pilotUrl = "https://de.dhv-xc.de/flights?" . FILTER_PARAMS . "&fkpil=" . $pilot->id;
 
 
-    </head>
-
-    <body>
-    <h2>XC Open 2022 Live Ranking</h2>
-    <table>
-        <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Freie Strecke</th>
-            <th>Dreieck</th>
-            <th>Airtime</th>
-            <th>Gesamt</th>
-        </tr>
-        <?php
-
-        $flightUrl = "https://de.dhv-xc.de/flight/";
-        $rank = 0;
-        foreach ($pilots as $pilot) {
-            $rank++;
-            $totalScore = $pilot->totalPoints;
-            $name = $pilot->name;
-
-            $airtimeScore = $pilot->airtimePoints;
-            $triangleScore = $pilot->trianglePoints;
-            $distanceScore = $pilot->distancePoints;
-            $distanceUrl = "#";
-            $triangleUrl = "#";
-            $airtimeUrl = "#";
-
-            if (!empty($pilot->distancePointsFlight)){
-                $distanceUrl = $flightUrl . $pilot->distancePointsFlight;
-            }
-
-            if (!empty($pilot->trianglePointsFlight)){
-                $triangleUrl = $flightUrl . $pilot->trianglePointsFlight;
-            }
-
-            if (!empty($pilot->airtimePointsFlight)){
-                $airtimeUrl = $flightUrl . $pilot->airtimePointsFlight;
-            }
-
-            $distanceCell = "<td><a href=\"$distanceUrl\">$distanceScore</a></td>";
-            if ($distanceUrl === "#"){
-                $distanceCell = "<td>$distanceScore</td>";
-            }
-
-            $triangleCell = "<td><a href=\"$triangleUrl\">$triangleScore</a></td>";
-
-            if ($triangleUrl === "#"){
-                $triangleCell = "<td>$triangleScore</td>";
-            }
-            $airtimeCell = "<td><a href=\"$airtimeUrl\">$airtimeScore</a></td>";
-
-            if ($airtimeUrl === "#"){
-                $airtimeCell = "<td>$airtimeScore</td>";
-            }
-
-            $pilotUrl = "https://de.dhv-xc.de/flights?" . FILTER_PARAMS . "&fkpil=" . $pilot->id;
-
-
-            $out = <<<HEREDOC
+        $out = <<<HEREDOC
 <tr>
-<td>$rank</td>
+<td>$displayRank</td>
 <td><a href="$pilotUrl">$name</a></td>
 $distanceCell
 $triangleCell
@@ -187,16 +190,16 @@ $airtimeCell
 
 HEREDOC;
 
-            echo $out;
+        echo $out;
 
 
-        }
+    }
 
-        ?>
-    </table>
+    ?>
+</table>
 
-    </body>
-    </html>
+</body>
+</html>
 <?php
 
 
@@ -208,19 +211,19 @@ class Pilot
         $this->flights = [];
     }
 
-    public  $id;
-    public  $name;
-    public  $flights;
+    public $id;
+    public $name;
+    public $flights;
 
-    public  $airtimePoints = 0;
-    public  $distancePoints = 0;
-    public  $trianglePoints = 0;
+    public $airtimePoints = 0;
+    public $distancePoints = 0;
+    public $trianglePoints = 0;
 
-    public  $totalPoints = 0;
+    public $totalPoints = 0;
 
-    public  $airtimePointsFlight = "";
-    public  $distancePointsFlight = "";
-    public  $trianglePointsFlight = "";
+    public $airtimePointsFlight = "";
+    public $distancePointsFlight = "";
+    public $trianglePointsFlight = "";
 
     public function getBestPoints()
     {
@@ -277,20 +280,20 @@ class Pilot
 
 class Flight
 {
-    public  $id;
-    public  $airtime;
+    public $id;
+    public $airtime;
 
-    public  $airtimePoints;
-    public  $trianglePoints;
-    public  $distancePoints;
+    public $airtimePoints;
+    public $trianglePoints;
+    public $distancePoints;
     public $landing;
 
     public function __construct($id)
     {
-        $arrContextOptions=array(
-            "ssl"=>array(
-                "verify_peer"=>false,
-                "verify_peer_name"=>false,
+        $arrContextOptions = array(
+            "ssl" => array(
+                "verify_peer" => false,
+                "verify_peer_name" => false,
             ),
         );
         $this->id = $id;
@@ -318,7 +321,7 @@ class Flight
     public function calcAirtimePoints()
     {
 
-        if ($this->landing !== "Merkur"){ //Merkur landings only
+        if ($this->landing !== "Merkur") { //Merkur landings only
             $this->airtimePoints = 0;
             return;
         }
